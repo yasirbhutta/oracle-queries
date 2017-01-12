034f6203202df32f8c06f75bd02cf42e9ff68783
yasirbhutta <yasirbhutta@gmail.com>
2017-01-12 21:05:46 +0500

    user

--- sequences/seq-evaluations.sql
+++ sequences/seq-evaluations.sql
@@ -2,11 +2,11 @@ CREATE SEQUENCE evaluations_sequence
 INCREMENT BY 1
 START WITH 1 ORDER;
 
-select evaluations_sequence.currval
-from dual;
-
-DROP SEQUENCE EVALUATIONS_SEQUENCE;
+--select evaluations_sequence.currval
+--from dual;
 --
---delete from students;
-INSERT INTO Students(STUDENT_ID,STUDENT_NAME)
-VALUES(evaluations_sequence.nextval,'Muhammad Ali');
+--DROP SEQUENCE EVALUATIONS_SEQUENCE;
+----
+----delete from students;
+--INSERT INTO Students(STUDENT_ID,STUDENT_NAME)
+--VALUES(evaluations_sequence.nextval,'Muhammad Ali');
